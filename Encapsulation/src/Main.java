4a3fc53800113e7f8eea73fdd49eaec8d53b5578
daruvurisai <daruvurisai@gmail.com>
2021-06-23 14:20:27 -0400

    Encapsulation

---
+++ Encapsulation/src/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args){
+       EnhancedPlayer player = new EnhancedPlayer("Sai",200,"Gun");
+        System.out.println("Initial Health is "+player.getHealth());
+    }
+}
